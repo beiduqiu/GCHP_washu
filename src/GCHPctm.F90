b4c58a8f7b89f436d55cfa070853ac21cd1ab8af
Wang <w.zifan1@compute1-client-1.ris.wustl.edu>
2024-03-23 21:52:55 -0500

    load balance component added

--- src/GCHPctm.F90
+++ src/GCHPctm.F90
@@ -23,10 +23,10 @@ Program GCHPctm_Main
    type (MAPL_CapOptions) :: cap_options
    integer                :: status
 
-   cap_options = MAPL_CapOptions(cap_rc_file='CAP.rc')
-   cap_options%logging_config = 'logging.yml'
-   cap = MAPL_CAP('GCHP', ROOT_SetServices, cap_options=cap_options)
-   call cap%run(_RC)
+   !cap_options = MAPL_CapOptions(cap_rc_file='CAP.rc')
+   !cap_options%logging_config = 'logging.yml'
+   !cap = MAPL_CAP('GCHP', ROOT_SetServices, cap_options=cap_options)
+   !call cap%run(_RC)
    _VERIFY(status)
 
  end Program GCHPctm_Main
